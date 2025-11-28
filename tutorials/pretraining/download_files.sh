cebc2d5b2a785c6df71121dee5e0f66785cc15ac
ChaoHan-UoS <hanchao1227@gmail.com>
2025-11-28 17:53:57 +0000

    Update bash script for downloading data examples

--- tutorials/pretraining/download_files.sh
+++ tutorials/pretraining/download_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 echo "Downloading MethylGPT data into data_examples/..."
 
 mkdir -p data_examples
@@ -13,13 +11,8 @@ DROPBOX_URL="https://www.dropbox.com/scl/fo/9113fa2h5jbrud2zcb7s9/ACXKuYZA5goHdH
 wget -O "$ZIP_PATH" "$DROPBOX_URL"
 
 echo "Extracting zip..."
-if unzip -q "$ZIP_PATH" -d data_examples/; then
-    echo "Extraction successful. Removing zip..."
-    rm "$ZIP_PATH"
-else
-    echo "⚠️  unzip failed. Keeping zip file for debugging."
-    echo "Try extracting manually with: unzip $ZIP_PATH -d data_examples/"
-    exit 1
-fi
+
+unzip -qo "$ZIP_PATH" -d data_examples/
+rm "$ZIP_PATH"
 
 echo "Done. Files are in data_examples/"
