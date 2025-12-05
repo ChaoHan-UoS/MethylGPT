0ae14f1ee1b68fc2b573853469f5237c5e98eda1
ChaoHan-UoS <hanchao1227@gmail.com>
2025-12-05 03:39:24 +0000

    Remove submission snapshot for Griffin batch job

--- bash_griffin/run_pretrain.sh
+++ bash_griffin/run_pretrain.sh
@@ -16,18 +16,11 @@ pwd
 echo ""
 echo "Begin"
 
-# Create a task-specific dir for the SLURM job output (at execution time)
-TASK_DIR="${DST_PROJ}/${SLURM_ARRAY_JOB_ID}/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
-mkdir -p $TASK_DIR
-
-# Mirror the pre-submission snapshot into the task-specific dir
-rsync -a --delete $SNAPSHOT_DIR/ $TASK_DIR/
-
 # Run the code from the task-specific dir
 cd "${TASK_DIR}/tutorials/pretraining"
 pwd
 
-echo "Running task ${SLURM_ARRAY_TASK_ID}"
+echo "Running task ${SLURM_JOB_ID}"
 
 # Activate Poetry venv
 VENV_PATH=$(poetry env info --path)
@@ -35,4 +28,4 @@ source "${VENV_PATH}/bin/activate"
 
 bash run_pretraining.sh
 
-echo "Done"
+echo "Task ${SLURM_JOB_ID} done"
--- bash_griffin/submit_job.sh
+++ bash_griffin/submit_job.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-SRC_DIR="/data/cep/chao.han/"
-DST_DIR="/scratch/wsspaces/chao.han-"
-PROJ="MethylGPT"
+BASE_DIR="/scratch/wsspaces/chao.han-"
+PROJ="MethylGPT/MethylGPT"
+TASK_DIR="$BASE_DIR$PROJ"
 
-# Dir where code is stored
-CODE_DIR="$SRC_DIR$PROJ"
-DST_PROJ="$DST_DIR$PROJ"
+mkdir -p "${TASK_DIR}/out"
 
-# Create a timestamped dir for the snapshot (at submission time)
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-SNAPSHOT_DIR="$DST_PROJ/Snapshot_$TIMESTAMP"
-
-# Mirror the current state of the code to the snapshot dir
-mkdir -p $SNAPSHOT_DIR
-rsync -a --delete $CODE_DIR/ $SNAPSHOT_DIR/
-
-# Submit the SLURM job, passing the snapshot dir as an argument
-# Run 1 task
-JOBID=$(sbatch --hold --parsable \
-               --output=${DST_PROJ}/%A/out/output_%A_%a.txt \
-               --export=DST_PROJ=$DST_PROJ,SNAPSHOT_DIR=$SNAPSHOT_DIR \
-               --array=0 \
-               ./bash_griffin/run_pretrain.sh)
-
-# Create parent dirs (job-level) for SLURM output
-mkdir -p "${DST_PROJ}/${JOBID}/out"
-scontrol release "$JOBID"
-echo "Submitted $JOBID"
+sbatch --output=${TASK_DIR}/out/output_%j.txt \
+       --export=TASK_DIR=TASK_DIR \
+       ./bash_griffin/run_pretrain.sh
