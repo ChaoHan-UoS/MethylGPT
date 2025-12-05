c42382dfe5bde42dcf584a2e13132449f9079233
ChaoHan-UoS <hanchao1227@gmail.com>
2025-12-05 03:44:51 +0000

    Debug on Griffin

--- tutorials/pretraining/run_pretraining.sh
+++ tutorials/pretraining/run_pretraining.sh
@@ -59,19 +59,19 @@ fi
 echo ""
 echo "--- Starting Training Step ---"
 
-python "${TRAIN_SCRIPT}" \
-    --config_file "${CONFIG_FILE}" \
-    --probe_id_path "${PROBE_ID_REF}" \
-    --parquet_data_dir "${PREPROCESSED_PARQUET_DIR}" \
-    --qced_metadata_path "${PREPROCESSED_METADATA_PATH}" \
-    "$@"           # additional args of the TRAIN_SCRIPT passed when invoking this bash script
-
-#torchrun --standalone --nproc_per_node=2 "${TRAIN_SCRIPT}" \
+#python "${TRAIN_SCRIPT}" \
 #    --config_file "${CONFIG_FILE}" \
 #    --probe_id_path "${PROBE_ID_REF}" \
 #    --parquet_data_dir "${PREPROCESSED_PARQUET_DIR}" \
 #    --qced_metadata_path "${PREPROCESSED_METADATA_PATH}" \
-#    "$@"
+#    "$@"           # additional args of the TRAIN_SCRIPT passed when invoking this bash script
+
+torchrun --standalone --nproc_per_node=2 "${TRAIN_SCRIPT}" \
+    --config_file "${CONFIG_FILE}" \
+    --probe_id_path "${PROBE_ID_REF}" \
+    --parquet_data_dir "${PREPROCESSED_PARQUET_DIR}" \
+    --qced_metadata_path "${PREPROCESSED_METADATA_PATH}" \
+    "$@"
 
 if [ $? -ne 0 ]; then
     echo "Training script exited with an error."
